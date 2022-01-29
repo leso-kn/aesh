8ed7c244f4429775284760b81d9a1f047abf5b40
leso-kn <info@lesosoftware.com>
2022-01-29 22:39:46 +0100

    Improved code quality

--- aesh.js
+++ aesh.js
@@ -32,25 +32,25 @@ const fileExts = [
 
 //
 
-let inputFilename = process.argv[2];
-
-let file = createReadStream(inputFilename);
-let identify = new (require('identify-stream'));
-
-file.pipe(identify);
-
-identify.on('complete', (streamType) =>
+async function command_add(filename)
 {
-    let mime = streamType
-             ? streamType.mime
-             : (fileExts.filter(ext => mime.match(ext[0]))[0] || [0,'text/plain'])[1];
+    let file = createReadStream(filename);
+    let identify = new (require('identify-stream'));
 
-    let usePreview = previewTypes.filter(pt => mime.match(pt)).length > 0;
+    file.pipe(identify);
+    let streamType = await new Promise(r => identify.on('complete', r));
+
+    let mime = streamType
+            ? streamType.mime
+            : (fileExts.filter(ext => mime.match(ext[0]))[0] || [0,'text/plain'])[1];
+
+    let usePreview = previewTypes.filter(pt => mime.match(pt)).length > 0; // used in template.htm
     //
 
     let output = createWriteStream('.enc.htm');
     let template = readFileSync(join(__dirname, 'template.htm')).split('{{contents}}');
 
+    //
     output.write(minify.minify(eval(`\`${template[0]}\``)));
 
     let key = crypto.randomBytes(32);
@@ -58,37 +58,38 @@ identify.on('complete', (streamType) =>
 
     let cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
 
-    identify.pipe(cipher).pipe(new Base64Encode()).pipe(output, { end: false }).on('unpipe', () =>
+    // Encrypt file contents
+    await new Promise(r => identify.pipe(cipher)
+                                   .pipe(new Base64Encode())
+                                   .pipe(output, { end: false })
+                                   .on('unpipe', r));
+
+    iv = iv.toString('base64');
+
+    //
+    await new Promise(r => output.write(minify.minify(eval(`\`${template[1]}\``)), r));
+    await new Promise(r => output.close(r));
+
+    // Upload constructed html file to IPFS
+    console.log('uploading file..');
+    let ret = execSync('ipfs add .enc.htm').toString();
+
+    if (ret.startsWith('added '))
     {
-        iv = iv.toString('base64');
+        // Success
+        let hash = ret.split(' ')[1];
+        let shareLink = `https://ipfs.io/ipfs/${hash}#${key.toString('base64')}`;
 
-        output.write(minify.minify(eval(`\`${template[1]}\``)), (err) =>
+        qrcode.generate(shareLink, { small: true }, (qrc) =>
         {
-            // Done
-            output.close(() =>
-            {
-                console.log('uploading file..');
-                let ret = execSync('ipfs add .enc.htm').toString();
-
-                if (ret.startsWith('added '))
-                {
-                    // Success
-                    let hash = ret.split(' ')[1];
-
-                    let shareLink = `https://ipfs.io/ipfs/${hash}#${key.toString('base64')}`;
-                    qrcode.generate(shareLink, { small: true }, (qrc) =>
-                    {
-                        console.log(qrc);
-                        console.log('Share available at: ' + shareLink + '\n');
-                    });
-                    execSync('ipfs pin add ' + hash);
-                    unlinkSync('.enc.htm');
-                }
-                else
-                {
-                    console.log(ret);
-                }
-            });
+            console.log(qrc);
+            console.log('Share available at: ' + shareLink + '\n');
         });
-    });
-});
+
+        execSync('ipfs pin add ' + hash);
+        unlinkSync('.enc.htm');
+    }
+    else { /* Error */ console.log(ret); }
+};
+
+command_add(process.argv[2]);
