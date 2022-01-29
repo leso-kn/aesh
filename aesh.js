4cab93fdccf509b865b5d71ad052061c48ea9fa7
leso-kn <info@lesosoftware.com>
2022-01-29 22:27:14 +0100

    Improved code structure

--- aesh.js
+++ aesh.js
@@ -1,6 +1,10 @@
+/*
+ * aesh - Lesosoftware 2022
+ */
+
 let crypto = require('crypto');
-const { createReadStream, createWriteStream, unlinkSync } = require('fs');
-const { basename } = require('path');
+const { createReadStream, createWriteStream, unlinkSync, readFileSync } = require('fs');
+const { basename, join } = require('path');
 const { Base64Encode } = require('base64-stream');
 const { execSync } = require('child_process');
 const qrcode = require('qrcode-terminal');
@@ -11,18 +15,18 @@ console.log('encryping contents..');
 
 // Any other mime-type will be offered for downloading
 const previewTypes = [
-  /^image\//,
-  /^video\//,
-  /^audio\//,
-  /^text\//,
-  /^application\/pdf/
+    /^image\//,
+    /^video\//,
+    /^audio\//,
+    /^text\//,
+    /^application\/pdf/
 ];
 
 // Mapping between known file extensions and mime-types.
 // Used when mime-type cannot be determined using identify-stream
 const fileExts = [
-  [/\.iso$/, 'application/x-cd-image'],
-  [/\.html?$/, 'text/html']
+    [/\.iso$/, 'application/x-cd-image'],
+    [/\.html?$/, 'text/html']
 ];
 
 //
@@ -36,95 +40,54 @@ file.pipe(identify);
 
 identify.on('complete', (streamType) =>
 {
-  let mime = streamType
-           ? streamType.mime
-           : (fileExts.filter(ext => mime.match(ext[0]))[0] || [0,'text/plain'])[1];
+    let mime = streamType
+             ? streamType.mime
+             : (fileExts.filter(ext => mime.match(ext[0]))[0] || [0,'text/plain'])[1];
 
-  let usePreview = previewTypes.filter(pt => mime.match(pt)).length > 0;
-  let output = createWriteStream('.enc.htm');
+    let usePreview = previewTypes.filter(pt => mime.match(pt)).length > 0;
+    //
 
-  output.write(`<!DOCTYPE html>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-Status: <span id=s>downloading file..</span>
-<style>
-  #s:not([ready]):after { content: ""; display: inline-block; width: .7em; height: .7em; border: .15em solid #68999f; animation: 2s spin linear infinite; margin-left: .3em; }
-  #s[error]:after { animation: none; border-color: #a24 !important; }
-  @keyframes spin { 0% { transform: rotate(0) } 100% { transform: rotate(360deg) }}
-</style>
-<script>
-var _key = document.location.hash.substring(1);
-document.location.hash = '';
-</script>
-<script id="content">
-let mimetype = '${mime}';
-let enc = '`);
+    let output = createWriteStream('.enc.htm');
+    let template = readFileSync(join(__dirname, 'template.htm')).split('{{contents}}');
 
-  let key = crypto.randomBytes(32);
-  let iv = crypto.randomBytes(16);
+    output.write(eval(`\`${template[0]}\``));
 
-  let cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+    let key = crypto.randomBytes(32);
+    let iv = crypto.randomBytes(16);
 
-  identify.pipe(cipher).pipe(new Base64Encode()).pipe(output, { end: false }).on('unpipe', () =>
-  {
-    output.write(`';
+    let cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
 
-//
-
-let iv = '${iv.toString('base64')}';
-window.onload = async function()
-{
-  try
-  {
-    let key = _key;
-    _key = null;
-
-    s.innerText = 'decrypting contents..';
-
-    key = await crypto.subtle.importKey('raw', Uint8Array.from(atob(key), c => c.charCodeAt(0)), { name: 'AES-CBC' }, false, ['decrypt']);
-
-    let blobURL = window.URL.createObjectURL(new Blob([
-      await crypto.subtle.decrypt(
-      { name: 'AES-CBC', iv: Uint8Array.from(atob(iv), c => c.charCodeAt(0)) }, key, Uint8Array.from(atob(enc), c => c.charCodeAt(0)))], { type: mimetype }));
-
-    setTimeout(() => { s.setAttribute('ready', ''); s.innerHTML = 'ready.</br></br>If your browser does not display the document automatically, you can try to launch the download manually using <a href="' + blobURL + '"${usePreview ? '' : ' download="' + basename(inputFilename).split('"').join('') + '"'}>this link</a>.'; }, 1000);
-    ${usePreview
-      ? 'document.location = blobURL;'
-      : `let a = document.createElement('a');
-    a.href = blobURL;
-    a.download = decodeURIComponent('${encodeURIComponent(basename(inputFilename))}');
-    a.click();`}
-  }
-  catch (err)
-  { s.innerText = 'Error: ' + err.toString(); s.setAttribute('error', ''); content.parentNode.removeChild(content); }
-}
-</script>
-`, (err) =>
+    identify.pipe(cipher).pipe(new Base64Encode()).pipe(output, { end: false }).on('unpipe', () =>
     {
-      // Done
-      output.close(() =>
-      {
-        console.log('uploading file..');
-        let ret = execSync('ipfs add .enc.htm').toString();
+        iv = iv.toString('base64');
 
-        if (ret.startsWith('added '))
+        output.write(eval(`\`${template[1]}\``), (err) =>
         {
-          // Success
-          let hash = ret.split(' ')[1];
+            // Done
+            output.close(() =>
+            {
+                console.log('uploading file..');
+                let ret = execSync('ipfs add .enc.htm').toString();
 
-          let shareLink = `https://ipfs.io/ipfs/${hash}#${key.toString('base64')}`;
-          qrcode.generate(shareLink, { small: true }, (qrc) =>
-          {
-            console.log(qrc);
-            console.log('Share available at: ' + shareLink + '\n');
-          });
-          execSync('ipfs pin add ' + hash);
-          unlinkSync('.enc.htm');
-        }
-        else
-        {
-          console.log(ret);
-        }
-      });
+                if (ret.startsWith('added '))
+                {
+                    // Success
+                    let hash = ret.split(' ')[1];
+
+                    let shareLink = `https://ipfs.io/ipfs/${hash}#${key.toString('base64')}`;
+                    qrcode.generate(shareLink, { small: true }, (qrc) =>
+                    {
+                        console.log(qrc);
+                        console.log('Share available at: ' + shareLink + '\n');
+                    });
+                    execSync('ipfs pin add ' + hash);
+                    unlinkSync('.enc.htm');
+                }
+                else
+                {
+                    console.log(ret);
+                }
+            });
+        });
     });
-  });
 });
